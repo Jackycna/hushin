7902287ce75cb48d01ec495fd8187fb90c5a70b2
jackycna <jackycna@gmail.com>
2025-05-22 23:41:11 +0530

    theme done

--- lib/features/auth/pages/get_started.dart
+++ lib/features/auth/pages/get_started.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hushin/core/colors/app_colors.dart';
+import 'package:hushin/data/common/widgets/basicapp_button.dart';
+import 'package:hushin/features/theme/style/theme_style.dart';
 
 class GetStarted extends StatefulWidget {
   const GetStarted({super.key});
@@ -12,14 +14,40 @@ class GetStarted extends StatefulWidget {
 class _GetStartedState extends State<GetStarted> {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         children: [
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back_ios, color: AppColors.primarycolor),
+          Positioned(
+            top: height * 0.05,
+            left: width * 0.05,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: AppColors.primarycolor,
+                size: 30,
+              ),
+            ),
+          ),
+          Positioned(
+            top: height * 0.1,
+            child: Container(
+              height: height * 0.6,
+              width: width,
+              child: Image.asset(
+                'assets/images/common/get.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.2,
+            left: width * 0.3,
+            child: BasicappButton(onpressed: () {}, title: 'Get Started'),
           ),
         ],
       ),
